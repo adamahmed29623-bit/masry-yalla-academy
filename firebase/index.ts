70a1b6f42b4d81ee1e781320ed0d0e23fe6899a5
adamahmed29623-bit <adamahmed29623@gmail.com>
2025-12-24 19:35:38 +0200

    Create index.ts

---
+++ firebase/index.ts
@@ -0,0 +1,11 @@
+'use client';
+
+// تصدير المحرك والـ Provider
+export * from './client';
+export * from './provider';
+
+// تصدير الأدوات الذكية من مجلد الـ firestore
+export * from './firestore/use-collection';
+export * from './firestore/use-document';
+export * from './firestore/use-memo-firebase';
+export * from './firestore/non-blocking-firestore';
